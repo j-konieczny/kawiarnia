d74b729c9896f8f79f5ed4758518a583fe3a0aab
j-konieczny <jakub.konieczny11@gmail.com>
2020-05-24 18:04:11 +0200

    add button

--- main-navigation.js
+++ main-navigation.js
@@ -9,3 +9,20 @@
     button.addEventListener("click", toggleClass);
     menu.addEventListener("click", toggleClass);
 })();
+
+// btn to top
+
+const btnScrollToTop = document.querySelector("#btnScrollToTop");
+
+btnScrollToTop.addEventListener("click", function(){
+    // window.scrollTo(0, 0);
+
+
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+
+    });
+});
+
