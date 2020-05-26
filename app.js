198982997bb55f0e24dbffc1d9122ec41b156d10
j-konieczny <jakub.konieczny11@gmail.com>
2020-05-26 21:45:24 +0200

    menu3

--- app.js
+++ app.js
@@ -129,62 +129,62 @@ const menu = [
   },
 ];
 
-const kawy = [
-  {
-    id: 1,
-    title: "Espresso",
-    category: "Kawa",
-    price: 15.99,
-    img: "images/espresso.jpg",
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
-  },
-  {
-    id: 2,
-    title: "Americano",
-    category: "Kawa",
-    price: 13.99,
-    img: "images/1.jpg",
-    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
-  },
-  {
-    id: 3,
-    title: "Drip v60 200/300ml",
-    category: "Kawa",
-    price: 6.99,
-    img: "images/3.jpg",
-    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
-  },
-  {
-    id: 4,
-    title: "Chemex 440ml",
-    category: "Kawa",
-    price: 20.99,
-    img: "./images/2.jpg",
-    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
-  },
-  {
-    id: 5,
-    title: "Latte",
-    category: "Kawa",
-    price: 22.99,
-    img: "./images/9.jpg",
-    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
-  },
-  {
-    id: 6,
-    title: "Cappuccino",
-    category: "Kawa",
-    price: 18.99,
-    img: "./images/8.jpg",
-    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
-  },
-];
+// const kawy = [
+//   {
+//     id: 1,
+//     title: "Espresso",
+//     category: "Kawa",
+//     price: 15.99,
+//     img: "images/espresso.jpg",
+//     desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+//   },
+//   {
+//     id: 2,
+//     title: "Americano",
+//     category: "Kawa",
+//     price: 13.99,
+//     img: "images/1.jpg",
+//     desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+//   },
+//   {
+//     id: 3,
+//     title: "Drip v60 200/300ml",
+//     category: "Kawa",
+//     price: 6.99,
+//     img: "images/3.jpg",
+//     desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+//   },
+//   {
+//     id: 4,
+//     title: "Chemex 440ml",
+//     category: "Kawa",
+//     price: 20.99,
+//     img: "./images/2.jpg",
+//     desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+//   },
+//   {
+//     id: 5,
+//     title: "Latte",
+//     category: "Kawa",
+//     price: 22.99,
+//     img: "./images/9.jpg",
+//     desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+//   },
+//   {
+//     id: 6,
+//     title: "Cappuccino",
+//     category: "Kawa",
+//     price: 18.99,
+//     img: "./images/8.jpg",
+//     desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+//   },
+// ];
 // get parent element
 const sectionCenter = document.querySelector(".section-center");
 const btnContainer = document.querySelector(".btn-container");
 // display all items when page loads
 window.addEventListener("DOMContentLoaded", function () {
-  diplayMenuItems(kawy);
+  diplayMenuItems(menu);
   displayMenuButtons();
 });
 
